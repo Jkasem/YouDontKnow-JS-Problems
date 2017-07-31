36bb7e160c1e55853143d3a7a686ed4b112e74a3
Justin Kaseman <justinkaseman@live.com>
2017-07-30 21:53:39 -0700

    Update Book 1 Chapter 1

---
+++ script.js
@@ -0,0 +1,39 @@
+//Up & Going
+//CH1
+  function oneOne() {
+
+    const TAX_RATE = 0.08;
+    const PHONE_PRICE = 99.99;
+    const ACCESSORY_PRICE = 14.99;
+    const SPENDING_THRESHOLD = 400;
+    let balance = prompt("How much money do you have?");
+    let total = 0;
+
+    function tax( amount ) {
+      return amount * TAX_RATE;
+    }
+
+    function priceFormat( amount ) {
+      return "$" + amount.toFixed( 2 );
+    }
+
+    while (total < balance) {
+       total = total + PHONE_PRICE;
+
+        if (total < SPENDING_THRESHOLD) {
+	          total = total + ACCESSORY_PRICE;
+        }
+    }
+
+    total = total + tax( total );
+
+    console.log (
+      "Your total is " + priceFormat( total )
+    );
+
+    if (total > balance) {
+      console.log(
+        "You can't afford this!"
+      );
+    }
+  }
